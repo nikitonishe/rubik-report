16723cbcd245029a625c1e6b6e8681cfd4d6f1d1
nikitandr <ns@indotech.ru>
2019-05-14 20:28:03 +0300

    fix

--- Report.js
+++ Report.js
@@ -3,30 +3,30 @@ const MongoDbCursor = require('../../services/MongoDbCursor');
 
 class Report extends Rubik.Kubik {
   /**
-   * Устанавливает зависимости и нициализирует tasks и volumes
-   * @param {Array|String} volumes Путь или массив путей до папки с заданиями
+   * Устанавливает зависимости и инициализируем csvGenerators и getSpecificQuery
+   * @param {Object} csvGenerators    Объект с классами формирующими csv
+   * @param {Object} getSpecificQuery Объект, ключами в котором является название модели,
+   *                                  a значением функция возвращающая специфические параметры
+   *                                  запроса. Если для модели не указана функция формирования
+   *                                  запрос, будет выполнен запрос по умолчанию. См метод getQuery
    */
   constructor ({ csvGenerators, getSpecificQuery }) {
     super();
-    this.name = 'Report';
+    this.name = 'report';
     this.csvGenerators = csvGenerators || {};
     this.getSpecificQuery = getSpecificQuery || {};
-    this.dependencies = ['log', 'http', 'storage'];
+    this.dependencies = ['log', 'storage'];
   }
 
   /**
    * Универсальный метод для формирования csv по существующей коллекции в базе.
-   * Чтобы добавить новый отчет, необходимо добавить соответствующий класс
-   * в объект StatsCsvGenerator. Если для поиска необходимо выполнить специфический
-   * запрос нужно добавить поле с ключем названия модели в объект this.getSpecificQuery,
-   * значением этого поля должна быть функция возвращающая специфические параметры запроса
    * @param  {Object}   stream     Поток в который писать отчет
    * @param  {Object}   modelName  Название модели по которой формируется статистика
    * @param  {Obejct}   options    Параметры для формирования запроса в базу данных
    * @return {undefined}
    */
   async writeCsvToStream ({ stream, modelName, options }) {
-    const Model = this.rubik.storageModels[modelName];
+    const Model = this.storage.models[modelName];
     if (!Model || !this.csvGenerators[modelName]) throw new Error('invalid modelName');
 
     const query = await this.getQuery({ modelName, options })
@@ -55,15 +55,16 @@ class Report extends Rubik.Kubik {
     return { createdAt: { $gte: from, $lt: to }, bot: options.bot};
   }
 
-
-  async getHttpMiddleware() {
-    return this.httpMiddleware.bind(this);
-  }
-
-  async httpMiddleware(req, res, next) {
+  /**
+   * Метод в который можно передать аргумент экспрессовской мидлвары
+   * @param  {Object}   req  Express request
+   * @param  {Object}   res  Express response
+   * @param  {Function} next Express middleware
+   */
+  async httpExpressMiddleware(req, res, next) {
     try {
       const { modelName } = req.params
-      const Model = this.rubik.storageModels[modelName];
+      const Model = this.storage.models[modelName];
       if (!Model || !this.csvGenerators[modelName]) return next();
 
       try {
@@ -72,7 +73,7 @@ class Report extends Rubik.Kubik {
       } catch (err) {
         res.write('Error!');
         console.error('CSV generating error', err);
-        // next нельзя использоать так как мы установили заголовки
+        // next нельзя использовать так как мы установили заголовки
       }
 
       res.end();
@@ -81,10 +82,19 @@ class Report extends Rubik.Kubik {
     }
   }
 
+  /**
+   * Чтобы добавить новый отчет, необходимо добавить соответствующий класс
+   * в объект csvGenerators. Если для поиска необходимо выполнить специфический
+   * запрос нужно добавить поле с ключем названия модели в объект getSpecificQuery,
+   * значением этого поля должна быть функция возвращающая специфические параметры запроса
+   * @param  {Object} extension Объект с новыми csvGenerators и getSpecificQuery
+   */
   use(extension) {
-    if (Array.isArray(extension.volumes)) {
-      for (const volume of extension.volumes) {
-        this.volumes.push(volume);
+    if (extension) {
+      for (const name of Object.keys(extension)) {
+        if (extension[name] && typeof(extension[name]) === 'object' && this[name]) {
+          Object.Assign(this[name], extension[name]);
+        }
       }
     }
     super.use(extension);
